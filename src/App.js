98b6401a193f86b69f20cd76b88b29c35ec03279
Priyanka <priyankakri8893@gmail.com>
2023-07-06 22:26:24 +0530

    react 2

--- src/App.js
+++ src/App.js
@@ -1,82 +1,26 @@
 import './App.css';
-import React, { useState } from 'react';
+import Video from './components/Video';
 
 function App() {
+  let obj = {
+    title: 'React JS tutorial',
+    views: '999K',
+    time: '1 year ago',
+    channel: 'Coder Dost',
+  };
   return (
-    <div className="App-header">
-      <Calculator/>
+    <div className="App">
+      <div>Videos</div>
+      <Video {...obj}></Video>
+      <Video title="Node JS tutorial" views="100K" time="1 month ago"></Video>
+      <Video
+        title="Mongo DB tutorial"
+        views="1M"
+        time="1 month ago"
+        channel="Coder Dost"
+      ></Video>
     </div>
   );
 }
 
-function Calculator() {
-  const [result, setResult] = useState('');
-
-  return (
-     <>
-        <div className='calculator'>
-           <Result value={result} />
-           <Result value={result} />
-           <Keyarea setResult={setResult} />
-        </div>
-     </>
-  );
-}
-
-function Result({ value }) {
-   return (
-      <>
-         <div className='result'>
-            {value}
-         </div>
-      </>
-   )
-}
-function Keyarea({ setResult }) {
-  const handleKeyClick = (value) => {
-     setResult(value);
-  };
-
-  return (
-     <>
-        <div className='keyarea'>
-           <Keybox add={0} onClick={handleKeyClick} />
-           <Keybox add={4} onClick={handleKeyClick} />
-           <Keybox add={8} onClick={handleKeyClick} />
-           <Keybox add={12} onClick={handleKeyClick} />
-           <Keybox add={16} onClick={handleKeyClick} />
-        </div>
-     </>
-  );
-}
-
-function Keybox({ add, onClick }) {
-   return (
-      <>
-         <div className='keybox'>
-            <Key num={1 + add} onClick={onClick} />
-            <Key num={2 + add} onClick={onClick} />
-            <Key num={3 + add} onClick={onClick} />
-            <Key num={4 + add} onClick={onClick} />
-         </div>
-      </>
-   )
-}
-
-function Key({ num, onClick }) {
-   const handleClick = () => {
-      onClick(num);
-   };
-
-   return (
-      <>
-         <div className='key' onClick={handleClick}>
-            {num}
-         </div>
-      </>
-   )
-}
-
-
 export default App;
-
