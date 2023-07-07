6fd7b4a702d44af0b8b05341c1c56fa90d8a8125
Priyanka <priyankakri8893@gmail.com>
2023-07-07 10:13:04 +0530

    form

--- src/App.js
+++ src/App.js
@@ -11,35 +11,64 @@ function App(){
 function Form(){
    return(
       <>
-         Name: <input type= "text"/>
-         <br/>
-         <br/>
-         Address: <textarea></textarea>
-         <br/>
-         <br/>
-         Gender: <input type="radio" value= "m" name="gender"/>Male
-         <input type="radio" value= "f" name="gender"/>Female
-         <br/>
-         <br/>
-         color: <input type="color" name="color"/>
-         <br/>
-         <br/>
-         Email: <input type="email" name="email"/>
-         <br/>
-         <br/>
-         password: <input type="password" name="password"/>
-         <br/>
-         <br/>
-         age: <input type="number" name="number"/>
-         <br/>
-         <br/>
-         skills: <input type="text" name="skill"/>
-         <br/>
-         <br/>
-         <input type="reset" value="Reset"/>
-         <br/>
-         <input type="submit" value="send"/>
-      </>
+  <div className="awesome" style={{ border: "1px solid red" }}>
+    <h2>Enquiry</h2>
+    <form>
+      Name: <input type="text" name="" placeholder="Enter your name here" />
+      <br />
+      <br />
+      Address: <textarea defaultValue={""} />
+      <br />
+      Gender: <input
+        type="radio"
+        defaultValue="m"
+        name="gender"
+        required=""
+      />{" "}
+      Male
+      <input type="radio" defaultValue="f" name="gender" /> Female
+      <br />
+      <br />
+      color: <input type="color" name="color" />
+      <br />
+      <br />
+      Email: <input type="email" name="email" />
+      <br />
+      <br />
+      password: <input type="password" name="password" />
+      <br />
+      <br />
+      age: <input type="number" name="number" />
+      <br />
+      <br />
+      skills: <input type="text" name="skill" />
+      <br />
+      <br />
+      Country:
+      <select>
+        <option>Please select a country</option>
+        <optgroup label="Asia">
+          <option value="India">India</option>
+          <option value="Shri">Shri lanka</option>
+        </optgroup>
+        <option value="Usa">USA</option>
+      </select>
+      <br />
+      <br />
+      Hobbies: <input type="checkbox" name="Hobbies" />
+      Dancing
+      <input type="checkbox" name="Hobbies" /> Singing
+      <br />
+      <br />
+      Image
+      <input type="file" name="image" />
+      <br />
+      <br />
+      <input type="submit" defaultValue="Send" name="" />
+      <input type="reset" defaultValue="Reset" name="" />
+    </form>
+  </div>
+</>
    )
 }
 
