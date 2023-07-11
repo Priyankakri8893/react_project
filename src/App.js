8f1294d38e1edf0fbbf517a405dbf3655bd7d5b0
Priyanka <priyankakri8893@gmail.com>
2023-07-11 21:49:50 +0530

    event

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
 
 function App(){
   let name= "Priyanka kumari"
-  let age= 23
   return(
     <>
       <h1>My name is {name}</h1>
-      <Greet naam= {name} age= {age}/>
+      <Greet/>
     </>
   )
 }
 
-function Age(props){
-  return(
-    <>
-      <h2>My sister age is {props.age}</h2>
-    </>
-  )
-}
-
-function Alertbuttom({message, children}){
-  return(
-    <>
-      <h1>{message}</h1>
-      <h1>{children}</h1>
-    </>
-  )
-}
-
-function Greet(props){
+function Greet(){
 
   function hello(){
-    console.log('hello')
+    alert('hello')
   }
 
   return(
     <>
-    <Alertbuttom message='How are you'/>
-    <Alertbuttom children='I am fine'/>
-
-    <Alertbuttom>What is your name</Alertbuttom>
-    <br/>
-    <br/>
-
-      <h2>hello {props.naam}, how are you?</h2>
-      <h2>My age is {props.age}</h2>
-      <Age age= {props.age}/>
-      <br/>
-      <button onClick={hello}>Click me</button>
+    <h1>{5+6} Priyanka</h1>
+    <button onClick={hello}>Click me</button>
     </>
   )
 }
