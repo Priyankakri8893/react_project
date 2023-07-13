fc3801eaa311097010bf8d664b1c069270a2c9d0
Priyanka <priyankakri8893@gmail.com>
2023-07-13 22:42:51 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 function App(){
   return(
@@ -13,11 +13,13 @@ function Add(){
   // let count= 1
   const [data, setData]= useState(1)
   const [value, setValue]= useState('hello')
+  const peristCount= useRef(1)
 
   function handleOnClick(){
     setData(data+1)
     // count= count+1
     // console.log(count)
+    peristCount.current= peristCount.current + 1
   }
 
   function hello(){
@@ -26,6 +28,7 @@ function Add(){
   return(
     <>
     <h1>{data}</h1>
+    <h1>{peristCount.current}</h1>
     {/* <p>{count}</p> */}
       <button onClick={handleOnClick}>add 1</button>
       <h2>{value}</h2>
