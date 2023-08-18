0fd3a90654584b379840e0e57c09f4ea0c4280c5
Priyanka <priyankakri8893@gmail.com>
2023-08-18 15:40:05 +0530

    menu card

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,8 @@
+import React from "react";
+import Resturant from "./component/Resturant";
 
-import { useState } from 'react'
-import './App.css'
+const App = () => {
+  return <Resturant/>;
+};
 
-function App() {
-  const [input, setInput]= useState("")
-  const [list, setList]= useState([])
-
-  const inputHandle= (e) => {
-    setInput(e.target.value)
-  }
-
-  const addList= () => {
-    setList( (prev) => {
-      return [...prev, input]
-    })
-    setInput("")
-  }
-
-  const deleteItem= (index) => {
-    setList( (prev) => {
-      const newList= [...prev]
-      newList.splice(index, 1)
-      return newList
-    })
-  }
-
-  const editItem= (index) => {
-    setInput(list[index])
-    setList( (prev) => {
-      const newList= [...prev]
-      newList.splice(index, 1)
-      return newList
-    })
-  }
-
-  
-
-  return (
-    <>
-      <input type="text" placeholder='add a items' onChange={inputHandle} value={input}/>
-      <button onClick={addList}>+</button>
-      <br />
-
-        {list.map( (item, index) => {
-          return (
-            <>
-            <button onClick={() => deleteItem(index)}>x</button>
-            <span key={index}>{item}</span>
-            <button onClick={ () => editItem(index)}>✍</button>
-            <br />
-            </>
-          )
-        })}
-    </>
-  )
-}
-
-export default App
+export default App;
